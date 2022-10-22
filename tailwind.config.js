dfaf8fd9b16ec2463c4c5c1eca5680cea1887d5f
Mustak Absar Khan <MustakHomeFolder@macs-MacBook-Pro.local>
2022-10-23 00:56:19 +0600

    Theme 1 UI responsiveness updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     },
 
     colors: {
+      // Theme 1
       // BACKGROUNDS
       t1mainBG: 'hsl(222, 26%, 31%)',
       t1toggleBG: 'hsl(223, 31%, 20%)',
@@ -33,6 +34,38 @@ module.exports = {
       //TEXT
       t1text1: 'hsl(221, 14%, 31%)',
       t1text2: 'hsl(0, 0%, 100%)',
+
+      //Theme 2
+      // BACKGROUNDS
+      t2mainBG: 'hsl(0, 0%, 90%)',
+      t2toggleBG: 'hsl(0, 5%, 81%)',
+      t2screenBG: 'hsl(0, 0%, 93%)',
+      // KEYS
+      t2keyBG: 'hsl(185, 42%, 37%)',
+      t2keySD: 'hsl(185, 58%, 25%)',
+      t2keyBGT: 'hsl(25, 98%, 40%)',
+      t2keySD1: 'hsl(25, 99%, 27%)',
+      t2keyBG1: 'hsl(45, 7%, 89%)',
+      t2keySD2: 'hsl(35, 11%, 61%)',
+      //TEXT
+      t2text1: 'hsl(60, 10%, 19%)',
+      t2text2: 'hsl(0, 0%, 100%)',
+
+      //Theme 3
+      // BACKGROUNDS
+      t3mainBG: 'hsl(268, 75%, 9%)',
+      t3toggleKeypadScreenBG: 'hsl(268, 71%, 12%)',
+      // KEYS
+      t3keyBG: 'hsl(281, 89%, 26%)',
+      t3keySD: 'hsl(285, 91%, 52%)',
+      t3keyBGT: 'hsl(176, 100%, 44%)',
+      t3keySD1: 'hsl(177, 92%, 70%)',
+      t3keyBG1: 'hsl(268, 47%, 21%)',
+      t3keySD2: 'hsl(290, 70%, 36%)',
+      //TEXT
+      t3text1: 'hsl(52, 100%, 62%)',
+      t3text2: 'hsl(198, 20%, 13%)',
+      t3text3: 'hsl(0, 0%, 100%)',
     }
   },
   plugins: [],
