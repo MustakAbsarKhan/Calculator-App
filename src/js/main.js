a3cdd406e9c29b1771581617014f0cd984f9be4d
Mustak Absar Khan <MustakHomeFolder@macs-MacBook-Pro.local>
2022-10-24 14:10:56 +0600

    UI Design Implemented

--- src/js/main.js
+++ src/js/main.js
@@ -9,41 +9,174 @@ const texttoprightSelector = document.getElementById("text-top-right-selector");
 const togglenumberSelector = document.getElementById("toggle-number-selector");
 const togglebarSelector = document.getElementById("toggle-bar-selector");
 const screeninputSelector = document.getElementById("screen-input-selector");
+const keypadSelector = document.getElementById("keypad-selector");
 
+const buttons = document.querySelectorAll(".button");
+const buttonSelector = [...buttons];
+
+const bigbuttonDark = document.querySelectorAll(".big-button");
+const bigbuttonDarkSelector = [...bigbuttonDark];
+
+const bigbuttonEqual = document.querySelector(".big-button-equal");
+// Theme 1 Toggle 1
 toggle1.addEventListener("click", () => {
   console.log("toggle 1 clicked");
+  bodySelector.classList.remove("bg-[#E6E6E6", "bg-[#17062A]");
+  bodySelector.classList.add("bg-t1mainBG");
+
+  texttopleftSelector.classList.remove("text-t2text1", "text-t3text1");
+  texttopleftSelector.classList.add("text-t1text2");
+
+  texttoprightSelector.classList.remove("text-t2text1", "text-t3text1");
+  texttoprightSelector.classList.add("text-t1keyBG1");
+
+  togglenumberSelector.classList.remove("text-t2text1", "text-t3text1");
+  togglenumberSelector.classList.add("text-t1text2");
+
+  //reduces time by looping and selecting each item of the array
+  toggleArray.forEach((element) => {
+    element.classList.remove("theme2-toggle-color", "theme3-toggle-color");
+    element.classList.add("theme1-toggle-color");
+  });
+
+  togglebarSelector.classList.remove("bg-[#D5CCCB]", "bg-[#1E0936]");
+  togglebarSelector.classList.add("bg-t1toggleBG");
+
+  screeninputSelector.classList.remove(
+    "bg-t2screenBG",
+    "text-t2text1",
+    "bg-t3toggleKeypadScreenBG",
+    "text-t3text1"
+  );
+  screeninputSelector.classList.add("bg-t1screenBG", "text-t1text2");
+
+  keypadSelector.classList.remove(
+    "bg-t2togglekpBG",
+    "bg-t3toggleKeypadScreenBG"
+  );
+  keypadSelector.classList.add("bg-t1toggleBG");
+
+  buttonSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme2-btn", "theme3-btn");
+    selectedButton.classList.add("theme1-btn");
+  });
+
+  bigbuttonDarkSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme2-big-button", "theme3-big-button");
+    selectedButton.classList.add("theme1-big-button");
+  });
+
+  bigbuttonEqual.classList.remove(
+    "theme2-big-red-button",
+    "theme3-big-red-button"
+  );
+  bigbuttonEqual.classList.add("theme1-big-red-button");
 });
 
-//Theme 2 toggle2
+//Theme 2 Toggle 2
 toggle2.addEventListener("click", () => {
   console.log("toggle 2 clicked");
-  bodySelector.classList.remove("bg-t1mainBG");
-  bodySelector.classList.add("bg-t2mainBG");
+  bodySelector.classList.remove("bg-t1mainBG", "bg-[#17062A]");
+  bodySelector.classList.add("bg-[#E6E6E6");
 
-  texttopleftSelector.classList.remove("text-t1text2");
+  texttopleftSelector.classList.remove("text-t1text2", "text-t3text1");
   texttopleftSelector.classList.add("text-t2text1");
 
-  texttoprightSelector.classList.remove("text-t1keyBG1");
+  texttoprightSelector.classList.remove("text-t1keyBG1", "text-t3text1");
   texttoprightSelector.classList.add("text-t2text1");
 
-  togglenumberSelector.classList.remove("text-t1text2");
+  togglenumberSelector.classList.remove("text-t1text2", "text-t3text1");
   togglenumberSelector.classList.add("text-t2text1");
 
   //reduces time by looping and selecting each item of the array
   toggleArray.forEach((element) => {
-    element.classList.remove("bg-t1keySD1", "active:bg-t1keyBGT");
-    element.classList.add("bg-t2keySD1", "active:bg-t2keyBGT");
+    element.classList.remove("theme1-toggle-color", "theme3-toggle-color");
+    element.classList.add("theme2-toggle-color");
   });
 
-  togglebarSelector.classList.remove("bg-t1toggleBG");
-  togglebarSelector.classList.add("bg-t2toggleBG");
+  togglebarSelector.classList.remove("bg-t1toggleBG", "bg-[#1E0936]");
+  togglebarSelector.classList.add("bg-[#D5CCCB]");
 
-  screeninputSelector.classList.remove("bg-t1screenBG", "text-t1text2");
-  screeninputSelector.classList.add("t2screenBG", "t2text1");
+  screeninputSelector.classList.remove(
+    "bg-t1screenBG",
+    "text-t1text2",
+    "bg-t3toggleKeypadScreenBG",
+    "text-t3text1"
+  );
+  screeninputSelector.classList.add("bg-t2screenBG", "text-t2text1");
+
+  keypadSelector.classList.remove("bg-t1toggleBG", "bg-t3toggleKeypad");
+  keypadSelector.classList.add("bg-t2togglekpBG");
+
+  buttonSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme1-btn", "theme3-btn");
+    selectedButton.classList.add("theme2-btn");
+  });
+
+  bigbuttonDarkSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme1-big-button", "theme3-big-button");
+    selectedButton.classList.add("theme2-big-button");
+  });
+
+  bigbuttonEqual.classList.remove(
+    "theme1-big-red-button",
+    "theme3-big-red-button"
+  );
+  bigbuttonEqual.classList.add("theme2-big-red-button");
 });
 
+// Theme 3 Toggle 3
 toggle3.addEventListener("click", () => {
   console.log("toggle 3 clicked");
-});
 
-document.getElementsByClassName;
+  bodySelector.classList.remove("bg-[#E6E6E6", "bg-t1mainBG");
+  bodySelector.classList.add("bg-[#17062A]");
+
+  texttopleftSelector.classList.remove("text-t2text1", "text-t1text2");
+  texttopleftSelector.classList.add("text-t3text1");
+
+  texttoprightSelector.classList.remove("text-t2text1", "text-t1keyBG1");
+  texttoprightSelector.classList.add("text-t3text1");
+
+  togglenumberSelector.classList.remove("text-t2text1", "text-t1text2");
+  togglenumberSelector.classList.add("text-t3text1");
+
+  //reduces time by looping and selecting each item of the array
+  toggleArray.forEach((element) => {
+    element.classList.remove("theme2-toggle-color", "theme1-toggle-color");
+    element.classList.add("theme3-toggle-color");
+  });
+
+  togglebarSelector.classList.remove("bg-[#D5CCCB]", "bg-t1toggleBG");
+  togglebarSelector.classList.add("bg-[#1E0936]");
+
+  screeninputSelector.classList.remove(
+    "bg-t2screenBG",
+    "text-t2text1",
+    "bg-t1screenBG",
+    "text-t1text2"
+  );
+  screeninputSelector.classList.add(
+    "bg-t3toggleKeypadScreenBG",
+    "text-t3text1"
+  );
+
+  keypadSelector.classList.remove("bg-t1toggleBG", "bg-t2togglekpBG");
+  keypadSelector.classList.add("bg-t3toggleKeypadScreenBG");
+
+  buttonSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme1-btn", "theme2-btn");
+    selectedButton.classList.add("theme3-btn");
+  });
+
+  bigbuttonDarkSelector.map((selectedButton) => {
+    selectedButton.classList.remove("theme1-big-button", "theme2-big-button");
+    selectedButton.classList.add("theme3-big-button");
+  });
+
+  bigbuttonEqual.classList.remove(
+    "theme1-big-red-button",
+    "theme2-big-red-button"
+  );
+  bigbuttonEqual.classList.add("theme3-big-red-button");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./*.{html,js}",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['League Spartan', ...defaultTheme.fontFamily.sans],
+        sans: ["League Spartan", ...defaultTheme.fontFamily.sans],
       },
       screens: {
-        'usm': {'max': '281px'},
+        usm: { max: "281px" },
         // => @media (max-width: 640px) { ... }
       },
     },
@@ -21,52 +18,52 @@ module.exports = {
     colors: {
       // Theme 1
       // BACKGROUNDS
-      t1mainBG: 'hsl(222, 26%, 31%)',
-      t1toggleBG: 'hsl(223, 31%, 20%)',
-      t1screenBG: 'hsl(224, 36%, 15%)',
+      t1mainBG: "hsl(222, 26%, 31%)",
+      t1toggleBG: "hsl(223, 31%, 20%)",
+      t1screenBG: "hsl(224, 36%, 15%)",
       // KEYS
-      t1keySD: 'hsl(224, 28%, 35%)',
-      t1keyBG: 'hsl(225, 21%, 49%)',
-      t1keyBGT: 'hsl(6, 63%, 50%)',
-      t1keySD1: 'hsl(6, 70%, 34%)',
-      t1keyBG1: 'hsl(30, 25%, 89%)',
-      t1keySD2: 'hsl(28, 16%, 65%)',
+      t1keySD: "hsl(224, 28%, 35%)",
+      t1keyBG: "hsl(225, 21%, 49%)",
+      t1keyBGT: "hsl(6, 63%, 50%)",
+      t1keySD1: "hsl(6, 70%, 34%)",
+      t1keyBG1: "hsl(30, 25%, 89%)",
+      t1keySD2: "hsl(28, 16%, 65%)",
       //TEXT
-      t1text1: 'hsl(221, 14%, 31%)',
-      t1text2: 'hsl(0, 0%, 100%)',
+      t1text1: "hsl(221, 14%, 31%)",
+      t1text2: "hsl(0, 0%, 100%)",
 
       //Theme 2
       // BACKGROUNDS
-      t2mainBG: 'hsl(0, 0%, 90%)',
-      t2toggleBG: 'hsl(0, 5%, 81%)',
-      t2screenBG: 'hsl(0, 0%, 93%)',
+      t2mainBG: "hsl(0, 0%, 90%)",
+      t2togglekpBG: "hsl(0, 5%, 81%)",
+      t2screenBG: "hsl(0, 0%, 93%)",
       // KEYS
-      t2keyBG: 'hsl(185, 42%, 37%)',
-      t2keySD: 'hsl(185, 58%, 25%)',
-      t2keyBGT: 'hsl(25, 98%, 40%)',
-      t2keySD1: 'hsl(25, 99%, 27%)',
-      t2keyBG1: 'hsl(45, 7%, 89%)',
-      t2keySD2: 'hsl(35, 11%, 61%)',
+      t2keyBG: "hsl(185, 42%, 37%)",
+      t2keySD: "hsl(185, 58%, 25%)",
+      t2keyBGT: "hsl(25, 98%, 40%)",
+      t2keySD1: "hsl(25, 99%, 27%)",
+      t2keyBG1: "hsl(45, 7%, 89%)",
+      t2keySD2: "hsl(35, 11%, 61%)",
       //TEXT
-      t2text1: 'hsl(60, 10%, 19%)',
-      t2text2: 'hsl(0, 0%, 100%)',
+      t2text1: "hsl(60, 10%, 19%)",
+      t2text2: "hsl(0, 0%, 100%)",
 
       //Theme 3
       // BACKGROUNDS
-      t3mainBG: 'hsl(268, 75%, 9%)',
-      t3toggleKeypadScreenBG: 'hsl(268, 71%, 12%)',
+      t3mainBG: "hsl(268, 75%, 9%)",
+      t3toggleKeypadScreenBG: "hsl(268, 71%, 12%)",
       // KEYS
-      t3keyBG: 'hsl(281, 89%, 26%)',
-      t3keySD: 'hsl(285, 91%, 52%)',
-      t3keyBGT: 'hsl(176, 100%, 44%)',
-      t3keySD1: 'hsl(177, 92%, 70%)',
-      t3keyBG1: 'hsl(268, 47%, 21%)',
-      t3keySD2: 'hsl(290, 70%, 36%)',
+      t3keyBG: "hsl(281, 89%, 26%)",
+      t3keySD: "hsl(285, 91%, 52%)",
+      t3keyBGT: "hsl(176, 100%, 44%)",
+      t3keySD1: "hsl(177, 92%, 70%)",
+      t3keyBG1: "hsl(268, 47%, 21%)",
+      t3keySD2: "hsl(290, 70%, 36%)",
       //TEXT
-      t3text1: 'hsl(52, 100%, 62%)',
-      t3text2: 'hsl(198, 20%, 13%)',
-      t3text3: 'hsl(0, 0%, 100%)',
-    }
+      t3text1: "hsl(52, 100%, 62%)",
+      t3text2: "hsl(198, 20%, 13%)",
+      t3text3: "hsl(0, 0%, 100%)",
+    },
   },
   plugins: [],
-}
+};
