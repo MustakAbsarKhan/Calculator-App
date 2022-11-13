36bd5d5ae64a021b346106f93a44251fd6139faa
Mustak Absar Khan <MustakHomeFolder@macs-MacBook-Pro.local>
2022-11-13 11:09:29 +0600

    Service Worker Install Test

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+// Test if service workers are supported
+
+if ("serviceWorker" in navigator) {
+  // Attempt to register it
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(function () {
+      // Success Message
+      console.log("ServiceWorker succesfully registered");
+    })
+    .catch(function (err) {
+      // Error Message
+      console.log("ServiceWorker registration failed: ", err);
+    });
+}
+
 const screenDisplay = document.getElementById("screen-input-selector");
 
 //Stores data in the local storage
